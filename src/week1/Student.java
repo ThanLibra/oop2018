843ba958b9dd771540dd319a166db8029ea34bfa
ThanLibra <41472635+ThanLibra@users.noreply.github.com>
2018-09-20 09:22:21 +0700

    Update Student.java

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -1,7 +1,20 @@
-package week1;
+
 
 public class Student {
 
+
+    private String n;
+    private String sid;
+    private String em;
+    public String getGr() {
+        return gr;
+    }
+
+    public void setGr(String gr) {
+        this.gr = gr;
+    }
+
+    private String gr;
     // TODO: khai báo các thuộc tính cho Student
 
     // TODO: khai báo các phương thức getter, setter cho Student
@@ -33,6 +46,33 @@ public class Student {
 
     String getInfo() {
         // TODO:
-        return null; // xóa dòng này sau khi cài đặt
+        return this.n + "\n" + this.sid + "\n" + this.em; // xóa dòng này sau khi cài đặt
     }
+
+
+    public String getN() {
+        return n;
+    }
+
+    public void setN(String n) {
+        this.n = n;
+    }
+
+    public String getSid() {
+        return sid;
+    }
+
+    public void setSid(String sid) {
+        this.sid = sid;
+    }
+
+    public String getEm() {
+        return em;
+    }
+
+    public void setEm(String em) {
+        this.em = em;
+    }
+
 }
+
