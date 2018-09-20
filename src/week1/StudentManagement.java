110c9791dbcae99448b1438b9ea9d9206fd7afc8
ThanLibra <41472635+ThanLibra@users.noreply.github.com>
2018-09-20 09:48:29 +0700

    Update StudentManagement.java

--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -1,8 +1,9 @@
-package week1;
+
 
 public class StudentManagement {
 
     // TODO: khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
+    Student[] students=new Student[100];
 
     public boolean sameGroup(Student s1, Student s2) {
         // TODO:
@@ -10,11 +11,34 @@ public class StudentManagement {
     }
 
     void studentsByGroup() {
-        // TODO:
+        int n=students.length;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(students[i].getGr().equals(students[j]))
+                {
+                    Student st;
+                    st=students[i+1];
+                    students[i+1]=students[j];
+                    students[j]=students[i+1];
+                }
+            }
+        }
+
     }
 
     void removeStudent(String id) {
         // TODO:
+        Student st = new Student();
+        for(int i=0;i<students.length;i++)
+        {
+            if(students[i].getSid()==id)
+            {
+                students[i]=st;
+                st=null;
+            }
+        }
     }
 
     public static void main(String[] args) {
