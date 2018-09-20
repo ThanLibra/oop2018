064cfccdc29df1f77cddd02cd656109b834beb68
ThanLibra <41472635+ThanLibra@users.noreply.github.com>
2018-09-20 09:08:31 +0700

    Update Task1.java

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -2,13 +2,24 @@ package week2.task1;
 
 public class Task1 {
 
+
     public static int gcd(int a, int b) {
         // TODO: Tính ước chung lớn nhất của 2 số a, b
-        return 0;
+        return (a == b) ? a :
+                (a > b) ? gcd(b, a - b) :
+                        (b > a) ? gcd(a, b - a) : 0;
+
     }
 
     public static int fibonacci(int n) {
         // TODO: Tìm số fibonacci ở vị trí n
-        return 0;
+        return (n == 0) ? 0 :
+                (n == 1) ? 1 :
+                        (n > 1) ? fibonacci(n - 1) + fibonacci(n - 2) :
+                                0;
+
     }
+
+
 }
+
