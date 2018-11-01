3ecddafed2ce0c078e9c7008f208207761e64689
ThanLibra <41472635+ThanLibra@users.noreply.github.com>
2018-11-01 08:24:01 +0700

    Update Task2.java

--- src/week7/Task2.java
+++ src/week7/Task2.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package week7.task2;
+package week7;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
