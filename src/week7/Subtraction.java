c88e16a8df3fdabbd4e449a1bf3435e7286a2d9a
ThanLibra <41472635+ThanLibra@users.noreply.github.com>
2018-11-01 08:27:13 +0700

    Update Subtraction.java

--- src/week7/Subtraction.java
+++ src/week7/Subtraction.java
@@ -3,9 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package week7.task1;
-
-/**
+package week7;
  *
  * @author HieuM */
 public class Subtraction extends BlnanyExpression{
