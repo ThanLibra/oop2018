d0f2b5b2fbf087c9b7eb866105ae0e5ebce3effb
ThanLibra <41472635+ThanLibra@users.noreply.github.com>
2018-11-15 06:51:13 +0700

    Update Task2.java

--- src/week10/Task2.java
+++ src/week10/Task2.java
@@ -1,4 +1,41 @@
 package week10;
 
+
+/**
+ *
+ * @author Admin
+ */
 public class Task2 {
+
+    public double[] sort(double[] arr,int n){
+        double tem;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n-1;j++)
+            {
+                if(arr[j]>arr[j+1])
+                {
+                    tem=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=tem;
+                }
+            }
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Task2 lap=new Task2();
+        double[]arr=new double[1000];
+        for(int i=0;i<1000;i++){
+            //arr[i]= random()*1000;
+            arr[i] = 1000-i;
+        }
+        arr=lap.sort(arr,1000);
+        for(int j=0;j<1000;j++)
+        {
+            System.out.println(arr[j]);
+        }
+    }
+
 }
