fcc350811f9aa15b61b3ca6d587196ec2b8300b1
ThanLibra <41472635+ThanLibra@users.noreply.github.com>
2018-11-22 05:47:50 +0700

    Update Task1.java

--- src/week11/Task1.java
+++ src/week11/Task1.java
@@ -1,4 +1,71 @@
 package week11;
 
+import java.util.ArrayList;
+
 public class Task1 {
+
+
+    public static class sortGeneric<T extends Comparable> {
+
+        private ArrayList<T> list;
+
+        public sortGeneric() {
+            list = new ArrayList<>();
+        }
+
+        public void add(T t) {
+            list.add(t);
+        }
+
+        public int size() {
+            return list.size();
+        }
+
+        public T get(int i) {
+            return list.get(i);
+        }
+
+        public void sort() {
+            T a;
+            for (int i = 0; i < list.size(); i++) {
+                for (int j = i + 1; j < list.size(); j++) {
+                    if (list.get(i).compareTo(list.get(j)) > 0) {
+                        a = list.get(i);
+                        list.set(i, list.get(j));
+                        list.set(j, a);
+                    }
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        //kieu du lieu nguyen thuy
+        sortGeneric list = new sortGeneric();
+        list.add(100);
+        list.add(7);
+        list.add(6);
+        list.add(20);
+        list.add(1);
+        list.add(15);
+        System.out.print("trc khi sap xep : ");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " ");
+        }
+        System.out.println();
+        list.sort();
+        System.out.print("sau khi sap xep: ");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " ");
+        }
+        System.out.println();
+
+
+    }
 }
+
+
+
+
+
